1b576671a1313ac92c196b260705ebb3d597a5c4
John Glatts <johnglatts1@hotmail.com>
2020-08-31 19:39:34 -0400

    Add files via upload

---
+++ LAB2/paycheck.c
@@ -0,0 +1,68 @@
+/*
+	ToDo
+	
+		- clean up and add more helpers in VS
+*/
+#include <stdio.h>
+
+/* Function Prototypes */
+int print_welcome_msg();
+void print_paycheck();
+
+/* Global Variables For Paycheck */
+int emp_num, salary, weekly_time, otime_amount;
+double otime_pay, net_pay, reg_pay;
+
+/* Main Function */
+int main(int argc, char* argv[]) {
+	if (print_welcome_msg()) {
+		print_paycheck();
+	}
+	return 0;
+}
+
+/* Prints the welcome message to stdout */
+int print_welcome_msg() {
+	printf("Welcome To \"TEMPLE HUMAN RESOURCES\"\n\n");
+	printf("\tEnter Employee Number: ");
+	scanf("%d", &emp_num);
+	if (emp_num <= 0) {
+		printf("\n\tThis is not a valid empolyee number.\n\tPlease run the program again.\n");
+		printf("\nThank you for using \"TEMPLE HUMAN RESOURCES\"\n");	// this needs to be in quotes
+		return 0;
+	}
+	printf("\tEnter Hourly Salary: ");
+	scanf("%d", &salary);
+	if (salary <= 0) {
+		printf("\n\tThis is not a valid hourly salary amount.\n\tPlease run the program again.\n");
+		printf("\nThank you for using \"TEMPLE HUMAN RESOURCES\"\n");
+		return 0;
+	}
+	printf("\tEnter Weekly Time: ");
+	scanf("%d", &weekly_time);
+	if (weekly_time <= 0) {
+		printf("\n\tThis is not a weekly time.\n\tPlease run the program again.\n");
+		printf("\nThank you for using \"TEMPLE HUMAN RESOURCES\"\n");
+		return 0;
+	}
+	return 1;
+}
+
+/* Prints the paycheck to stdout */
+void print_paycheck() {
+	printf("\t=================================\n");
+	printf("\tEmployee Number: %d\n", emp_num);
+	printf("\tHourly Salary: $%d\n", salary);
+	printf("\tWeekly Time: %d\n", weekly_time);
+	if (weekly_time > 40) {
+		reg_pay = 40 * salary;
+		otime_pay = (1.5 * salary) * (weekly_time - 40);
+	}
+	else reg_pay = weekly_time * salary;
+	printf("\tRegular Pay: $%.1f\n", reg_pay);
+	printf("\tOvertime Pay: $%.1f\n", otime_pay);
+	printf("\tNet Pay: $%.1f\n", reg_pay + otime_pay);
+	printf("\t=================================\n"); 
+	printf("Thank you for using \"TEMPLE HUMAN RESOURCES\"\n");
+}
+
---
+++ LAB2/revenue.c
@@ -0,0 +1,46 @@
+/*
+	Bang this guy out
+
+	not working on cis-linux2, 
+	getting no output 
+
+	works on windows
+
+*/
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	int item_price, quantity;
+	double discount;
+
+	printf("Welcome To Temple Store\n");
+	printf("\n\tEnter item price: ");
+	scanf("%d", &item_price);
+	if (item_price <= 0) {
+		printf("\n\tThis is not a valid item price.\n\tPlease run the program again.\n");
+		printf("\nThank you for using Temple Store\n");
+		return 1;
+	}
+	
+	printf("\tEnter quantity: ");
+	scanf("%d", &quantity);
+	if (quantity <= 0) {
+		printf("\n\tThis is not a valid quantity order.\n\tPlease run the program again.\n");
+		printf("\nThank you for using Temple Store\n");
+		return 1;
+	}
+	
+	if (quantity >= 50 || quantity <= 99)        discount = 0.1;
+	else if (quantity >= 100 || quantity <= 149) discount = 0.15;
+	else if (quantity >= 150)                    discount = 0.25;
+
+	printf("\n\tThe item price is: $%d\n", item_price);
+	printf("\tThe order is: %d item(s)\n", quantity);
+	printf("\tThe cost is: $%d\n", item_price * quantity);
+	printf("\tThe discount is: %.1f%%\n", discount * 100);
+	printf("\tThe discount amount is: $%.1f\n", discount * (item_price *  quantity));
+	printf("\tThe total is: $%.1f\n", (item_price * quantity) - (discount * (item_price * quantity)));
+	printf("\nThank you for using Temple Store\n");
+
+	return 0;
+}
