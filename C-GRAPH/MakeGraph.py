dc5346aab67b937fa1bcda7af5e94b87745c20c3
John Glatts <john@z-axiscc.com>
2020-10-28 08:56:26 -0400

    Update MakeGraph.py

--- C-GRAPH/MakeGraph.py
+++ C-GRAPH/MakeGraph.py
@@ -1,11 +1,12 @@
+import sys
 
-def makeGraph(nodes, edges, filename): 
-	f = open(filename, "w")
-	f.write(str(nodes) + " " + str(edges) + "\n")
-	for i in range(nodes):
-		f.write(str(i+1) + " " + str(((i * 156) % 25) + 9))	 #replace w/ random ints
-		f.write("\n")
-	f.close()
+def makeGraph(nodes, edges, filename):
+        f = open(filename, "w")
+        f.write(str(nodes) + " " + str(edges) + "\n")
+        for i in range(nodes):
+                f.write(str(i+1) + " " + str(((i+1 * 156) % nodes) + 1))
+                f.write("\n")
+        f.close()
 
-
-makeGraph(100, 100, "py_graph.txt")
+if __name__ == "__main__":
+        makeGraph(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
