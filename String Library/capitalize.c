ee8073ff5cd29a5e8f864436a4140bf7aa7192b4
John Glatts <john@z-axiscc.com>
2020-12-02 20:39:32 -0500

    Update capitalize.c

--- String Library/capitalize.c
+++ String Library/capitalize.c
@@ -5,10 +5,6 @@ void capitalize(char *s) {
     int index = 0;
     if ((*s >= 97) && (*s <= 122)) *s = *s - 32;
     while (*s) {
-       /*
-        * if the current character is a space then change the next character
-        * if it exists
-        */
        if (*s == 32) {
            if (*(s+1) != '\0') {
                *(s+1) = *(s+1) - 32;
