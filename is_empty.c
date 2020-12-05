4bacfed252f232ba06bedc5341c96a9324336883
John Glatts <john@z-axiscc.com>
2020-12-05 17:54:59 -0500

    Update is_empty.c

--- is_empty.c
+++ is_empty.c
@@ -3,11 +3,7 @@
 
 int is_empty(char* s) {
 	if (s == NULL)  return 1;
-	if (*s == '\0') return 1;
-	int len = 0;
-	for(len; s[len] != '\0'; ++len) { }
-	for (int i = 0; i < len; ++i) {
-		if (s[i] != ' ') return 0;
-	}
-	return 1; // if we make it to here, only whitespace is in the string
+	if (*s == '\0') return 1;;
+	for (int i = 0; i < _strlen(s); ++i) if (s[i] != ' ') return 0;
+	return 1; // if we make here, only whitespace is in the string
 }
