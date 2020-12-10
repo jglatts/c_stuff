976e5b49c34a9f9ff0bbf3a3c9dd61fbc9e9fd0f
John Glatts <john@z-axiscc.com>
2020-12-10 08:19:09 -0500

    Update queue.c

--- Queue Library/queue.c
+++ Queue Library/queue.c
@@ -7,16 +7,16 @@ Queue* make_new_queue() {
         Queue* q = (Queue*)malloc(sizeof(Queue));
         for (int i = 0; i < MAX_QUEUE_SIZE; ++i) q->queue[i] = 0;
         q->size = 0;
-	q->index = 0;
+        q->index = 0;
         return (q != NULL) ? q : NULL;
 }
 
 void enqueue(Queue* q, int d) {
         int copy = q->size;
         q->queue[q->size++] = d;
-	#ifdef USE_VERBOSE
+        #ifdef USE_VERBOSE
         printf("Enqueued %d, queue size = %d\n", q->queue[copy], copy+1);
-	#endif 
+        #endif
 }
 
 int dequeue(Queue* q) {
@@ -26,7 +26,7 @@ int dequeue(Queue* q) {
                 return q->queue[0];
         }
         q->size--;
-	return q->queue[q->index++];
+        return q->queue[q->index++];
 }
 
 int is_empty(Queue* q) {
@@ -35,18 +35,20 @@ int is_empty(Queue* q) {
 
 void test_queue() {
         Queue* q = make_new_queue();
-        int n, k;
+        int n = 0, c = 0;
         srand((unsigned)time(NULL));
         printf("------------JDG STACK------------\n");
         printf("How Many Elements In The Queue? -> ");
         scanf("%d", &n);
-        for (int i = 0; i < n; ++i) {
-                enqueue(q, rand() % n);
-        }
-        printf("\n%s", "Items on queue: ");
+        for (int i = 0; i < n; ++i) enqueue(q, rand() % n);
+        puts("\nItems on queue: ");
         while (!is_empty(q)) {
-                printf("%d ", dequeue(q));
+                printf(GREEN);
+                printf("%5d", dequeue(q));
+                printf(RESET);
+                //if (c == 0) continue;
+                c++;
+                if (c % 10 == 0) puts("");
         }
         puts("");
 }
-
