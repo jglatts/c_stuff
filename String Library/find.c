72cece462e7c3029dc194240eb1db7e9b911b3c7
John Glatts <john@z-axiscc.com>
2020-12-11 09:55:45 -0500

    Update find.c

--- String Library/find.c
+++ String Library/find.c
@@ -1,9 +1,8 @@
 #include "string.h"
 
 int find(char* h, char* n) {
-	int ret = -1, len = 0;
-	for(len; h[len] != '\0'; ++len) { }
-	for (int i = 0; i < len; ++i) {
+	int ret = -1;
+	for (int i = 0; i < _strlen(h); ++i) {
 		if (h[i] == *n) {
 			ret = i;
 			break;
