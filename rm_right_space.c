b092433f12f7aad37c97505aa075aee81f8a36a9
John Glatts <john@z-axiscc.com>
2020-12-13 07:26:20 -0500

    Update rm_right_space.c

--- rm_right_space.c
+++ rm_right_space.c
@@ -2,19 +2,12 @@
 #include <stdio.h>
 
 void rm_right_space(char* s) {
-	// add " " to know where the spaces are
-        int len = 0, count = 0, first_space_index = 0;
-        for (len; s[len] != '\0'; ++len) { }
-        for (int i = 0; i < len; ++i) {
+        int count = 0, first_space_index = 0;
+        for (int i = 0; i < _strlen(s); ++i) {
                 if (s[i] == ' ') {
                         first_space_index = i;
                         break;
                 }
         }
-	/*
-        for (int i = first_char_index; s[i] != '\0'; ++i) {
-                if (count < len - first_char_index) s[count++] = s[i];
-        }
-	*/
         s[first_space_index] = '\0';
 }
