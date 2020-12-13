ad53791c67b0a46036d35f85c16d1ba690992f4a
John Glatts <john@z-axiscc.com>
2020-12-13 08:15:05 -0500

    Update stack.h

--- Stack Library/stack.h
+++ Stack Library/stack.h
@@ -2,13 +2,16 @@
 #define STACK_H
 
 #define MAX_STACK_SIZE 100
+#define RED    "\033[1;31m"
+#define GREEN  "\033[0;32m"
+#define RESET  "\033[0m"
 
 typedef struct {
-	int stack[MAX_STACK_SIZE];
-	int size;
+        int *stack;
+        int size;
 } Stack;
 
-Stack* make_new_stack();
+Stack* make_new_stack(int);
 void push(Stack*, int);
 void test_stack();
 int pop(Stack*);
